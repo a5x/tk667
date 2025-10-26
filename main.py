e82252d934c6a3444a18517eb1eb0df42ce002df
A5X <134241855+a5x@users.noreply.github.com>
2025-10-26 19:59:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ except Exception:
 APP_TITLE = "667 SCRAPER"
 APP_MIN_SIZE = (1024, 640)
 
-LOCAL_VERSION = "2.5"
+LOCAL_VERSION = "2.4"
 GITHUB_OWNER  = "a5x"
 GITHUB_REPO   = "tk667"
 GITHUB_BRANCH = "main"
@@ -871,4 +871,5 @@ class App(tk.Tk):
 if __name__ == "__main__":
     app = App()
     app.mainloop()
+
 
